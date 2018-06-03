de9536965bb6a21d37067a04293800f1714fecea
Tsukatsuki Kanade <i@sukitsuki.com>
2018-06-04 01:11:45 +0700

    Fix Disqus reload problem

--- source/js/src/pjax.js
+++ source/js/src/pjax.js
@@ -4,9 +4,18 @@ document.addEventListener("pjax:send", function () {
 document.addEventListener("pjax:complete", function () {
   NexT.motion.integrator.reset(1);
   CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
+  if (window.DISQUS) {
+    DISQUS.reset({
+      reload: true,
+      config: disqus_config
+    });
+  } else {
+    loadComments();
+  }
 });
 
 document.addEventListener("pjax:error", function () {
+  console.log("pjax:error");
 });
 
 document.addEventListener("pjax:success", function () {
@@ -15,7 +24,7 @@ document.addEventListener("pjax:success", function () {
 document.addEventListener("DOMContentLoaded", function () {
   new Pjax({
     elements: ["a[href]:not(.fancybox)"],
-    selectors: [".content", ".sidebar", ".content-wrap", ".menu"],
+    selectors: [".content", ".sidebar", ".content-wrap", ".menu", ".comments"],
     cacheBust: false
     // currentUrlFullReload: true,
   });
