b3ae0920de1e66c1f26d5b71409dc4e6c3e84850
Tsukatsuki Kanade <i@sukitsuki.com>
2018-06-06 17:31:23 +0700

    Add aplayer and music

---
+++ source/js/src/aplayer.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    // mini: false,
+    autoplay: true,
+    theme: '#FADFA3',
+    loop: 'all',
+    order: 'random',
+    preload: 'auto',
+    volume: 0.5,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: '90px',
+    lrcType: 3,
+    audio: [
+      {
+        name: '潛別離',
+        artist: '銀臨',
+        url: 'player/n-i770aRaTo.m4a',
+        cover: 'player/n-i770aRaTo.jpg',
+        lrc: 'player/n-i770aRaTo.lrc',
+        theme: '#ebd0c2'
+      }
+    ]
+  });
+});
