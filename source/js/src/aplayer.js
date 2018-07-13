dd51b5e5164347a5c1ecac6ae1c0d77b101205a2
YumeMichi <do4suki@gmail.com>
2018-07-13 18:31:55 +0800

    Update music
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- source/js/src/aplayer.js
+++ source/js/src/aplayer.js
@@ -15,25 +15,12 @@ $(document).ready(function () {
     lrcType: 3,
     audio: [
       {
-        name: '潛別離',
-        artist: '銀臨',
-        url: '/player/n-i770aRaTo.m4a',
-        cover: '/player/n-i770aRaTo.jpg',
-        lrc: '/player/n-i770aRaTo.lrc',
+        name: 'オレンジ',
+        artist: '7!!',
+        url: '/player/01.m4a',
+        cover: '/player/01.jpg',
+        lrc: '',
         theme: '#ebd0c2'
-      }, {
-        name: '杏花弦外雨',
-        artist: '司夏',
-        url: '/player/io6iWBmE9hs.m4a',
-        cover: '/player/io6iWBmE9hs.jpg',
-        lrc: '/player/io6iWBmE9hs.lrc',
-        theme: '#b6eb86'
-      }, {
-        name: '生きていたんだよな',
-        artist: 'あいみょん',
-        url: '/player/180xCt99Lec.m4a',
-        lrc: '/player/180xCt99Lec.lrc',
-        theme: '#384e65'
       }
     ]
   });
